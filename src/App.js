a707c31b77d0681dfd944d2f85ae4caec8eced3b
Andrzej Tracz <andrzejtracz@MacBook-Air-Andrzej.local>
2024-01-19 00:42:16 +0100

    Nowe funkcjonalności i poprawki, gotowa wersja.

--- src/App.js
+++ src/App.js
@@ -11,15 +11,29 @@ function App() {
     {rowNumber: 4, rowDescription: 'Charge phone battery', rowAssigned: 'User Three'}
   ])
 
-  const addTodo = () => {
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
+  const addTodo = (description, assigned) => {
+    let rowNumber = 0;
     if(todos.length > 0){
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    }
+    else{
+      rowNumber = 1;
+    }
       const newTodo = {
-        rowNumber: todos.length + 1,
-        rowDescription: 'New Todo',
-        rowAssigned: 'User Three'
+        rowNumber: rowNumber,
+        rowDescription: description,
+        rowAssigned: assigned
       }
       setTodos(todos => [...todos, newTodo])
-    }
+  }
+  
+  const deleteTodo = (deleteTodoRowNumber) => {
+    let filtered = todos.filter(function(value){
+      return value.rowNumber !== deleteTodoRowNumber;
+    })
+    setTodos(filtered);
   }
 
   return (
@@ -29,11 +43,11 @@ function App() {
           Your Todo's
         </div>
         <div className='card-body'>
-          <TodoTable todos = {todos}/>
-          <button className='btn btn-primary' onClick={addTodo}>
-          Add new todo
+          <TodoTable todos = {todos} deleteTodo = {deleteTodo}/>
+          <button onClick={() => setShowAddTodoForm(!showAddTodoForm)} className='btn btn-primary'>
+            {showAddTodoForm ? 'Close New Todo' : 'New Todo'}
           </button>
-          <NewTodoForm/>
+          {showAddTodoForm && <NewTodoForm addTodo = {addTodo}/>}
         </div>
       </div>
     </div>
--- src/Components/NewTodoForm.js
+++ src/Components/NewTodoForm.js
@@ -1,19 +1,27 @@
 import { useState } from "react"
 
-function newTodoForm(){
+function NewTodoForm(props){
 
 const [description, setDescription] = useState('');
 const [assigned, setAssigned] = useState('');
 
-const desciptionChange = (event) => {
-    console.log('description', event.target.value)
-    setDescription(event.target.value)
+const submitTodo = () => {
+    if(description !== '' && assigned !== ''){
+        props.addTodo(description, assigned);
+        setAssigned('')
+        setDescription('')
+    }
 }
 
-const assignedChange = (event) => {
-    console.log('assigned', event.target.value)
-    setAssigned(event.target.value)
-}
+// const desciptionChange = (event) => {
+//     console.log('description', event.target.value)
+//     setDescription(event.target.value)
+// }
+
+// const assignedChange = (event) => {
+//     console.log('assigned', event.target.value)
+//     setAssigned(event.target.value)
+// }
 
     return(
         <div className='mt-5'>
@@ -24,7 +32,7 @@ const assignedChange = (event) => {
                         type='text' 
                         className='form-control' 
                         required
-                        onChange={assignedChange}
+                        onChange={e => setAssigned(e.target.value)}
                         value={assigned}
                     ></input>
                 </div>
@@ -34,14 +42,14 @@ const assignedChange = (event) => {
                         className='form-control' 
                         rows={3} 
                         required
-                        onChange={desciptionChange}
+                        onChange={e => setDescription(e.target.value)}
                         value={description}
                     ></textarea>
                 </div>
-                <button type='button' className='btn btn-primary mt-3'>Add todo</button>
+                <button type='button' className='btn btn-primary mt-3' onClick={submitTodo}>Add todo</button>
             </form>
         </div>
     )
 }
 
-export default newTodoForm
+export default NewTodoForm
